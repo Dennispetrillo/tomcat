544dcb798b4d02955c98bb3ef185363999fd9eba
Dennis Petrillo <dennis.petrillo@yahoo.com>
2017-11-18 19:01:12 -0500

    add tests and resources for the download, and extract and partial update of permissions

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -6,13 +6,41 @@
 
 package 'java-1.7.0-openjdk-devel' 
 
-group 'tomcatgroup' do
+group 'tomcat' do
   append true
 end
 
 user 'tomcat' do
-  group 'tomcatgroup'
+  group 'tomcat'
   home '/opt/tomcat'
   shell '/bin/nologin'
   manage_home false
+end
+
+directory '/opt/tomcat' do
+  action :create
+end 
+
+remote_file 'apache-tomcat-8.5.23.tar.gz' do
+  source 'http://supergsego.com/apache/tomcat/tomcat-8/v8.5.23/bin/apache-tomcat-8.5.23.tar.gz'
+#  owner 'web_admin'
+#  group 'web_admin'
+#  mode '0755'
+  action :create
+end
+
+execute 'unzip tar' do
+  command 'tar xvf apache-tomcat-8*tar.gz -C /opt/tomcat --strip-components=1'
+end
+
+#directory 'opt/tomcat' do
+#  group 'tomcat'
+#end
+
+execute 'change group recursively' do
+  command 'chgrp -R tomcat /opt/tomcat'
+end
+
+execute 'update mode recursively on conf' do
+  command 'chmod -R g+r /opt/tomcat/conf'
 end
--- test/smoke/setup/setup_test.rb
+++ test/smoke/setup/setup_test.rb
@@ -2,7 +2,7 @@ describe package('java-1.7.0-openjdk-devel') do
  it { should be_installed }
 end
 
-describe group('tomcatgroup') do
+describe group('tomcat') do
   it { should exist }
 end
 
@@ -11,26 +11,27 @@ describe user('tomcat') do
 end
 
 describe user('tomcat') do
-  its('group') { should eq 'tomcatgroup' }
+  its('group') { should eq 'tomcat' }
 end
 
 describe user('tomcat') do
   its('shell') { should eq '/bin/nologin' } 
 end
 
-#describe file('/etc/yum.repos.d/mongodb-org-3.4.repo') do
-# its ( 'size') { should_not be 0 }
+describe directory('/opt/tomcat') do
+  it { should exist }
+end
+
+describe file('/apache-tomcat-8.5.23.tar.gz') do
+  it { should exist }
+end
+
+#describe directory('opt/tomcat') do
+#  its('group') {should eq 'tomcat' }
 #end
 
-#describe package('mongodb-org') do
-#  it { should be_installed }
+#describe file('opt/tomcat/conf') do
+#  its('mode') { should cmp '00740' }
 #end
-
-#describe service ('mongod') do
-#  it { should be_running }
-#end
-
-#describe file('/var/log/mongodb/mongod.log') do
-#  its ('content') { should match /waiting for connections/ }
-#end
+  
 
